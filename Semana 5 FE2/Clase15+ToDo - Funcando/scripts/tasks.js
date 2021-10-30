f84fa5839a6db0db67ae4294da03843d3e437302
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2021-10-30 00:27:50 -0300

    Código feíto pero funcionando hasta eliminar tarea

--- Semana 5 FE2/Clase15+ToDo - Funcando/scripts/tasks.js
+++ Semana 5 FE2/Clase15+ToDo - Funcando/scripts/tasks.js
@@ -1,77 +1,138 @@
+/* ------------------------- Captura elementos HTML ------------------------- */
+const formTasks = document.forms[0];
+const mostrarUsuario = document.querySelector("#mostrarUsuario");
+const btnNuevaTarea = document.querySelector("#btnNuevaTarea");
+const tareasPendientes = document.querySelector("ul.tareas-pendientes");
+const tareasTerminadas = document.querySelector("ul.tareas-terminadas");
+const skeleton = document.querySelector("#skeleton");
+const errorMessage = document.querySelector(".errorMessage");
+const btnCerrarSesion = document.querySelector("#closeApp");
+
+/* ----------------------- Variables de uso frecuente ----------------------- */
+const apiUrlGetMe = "https://ctd-todo-api.herokuapp.com/v1/users/getMe";
+const apiUrlTasks = "https://ctd-todo-api.herokuapp.com/v1/tasks";
+const apiUrlDelete = "https://ctd-todo-api.herokuapp.com/v1/tasks/";
+const jwt = localStorage.getItem("token");
+
+/* --------------------------- Carga de la página --------------------------- */
+
 window.addEventListener("load", function () {
-  /* -------------------------------------------------------------------------- */
-  /*                        Funcionalidades de las Tasks                        */
-  /* -------------------------------------------------------------------------- */
-
-  /* ------------------------- Captura elementos HTML ------------------------- */
-  const formTasks = document.forms[0];
-  const mostrarUsuario = document.querySelector("#mostrarUsuario");
-  const btnNuevaTarea = document.querySelector("#btnNuevaTarea");
-  const tareasPendientes = document.querySelector("ul.tareas-pendientes");
-  const tareasTerminadas = document.querySelector("ul.tareas-terminadas");
-  const skeleton = document.querySelector("#skeleton");
-  const errorMessage = document.querySelector(".errorMessage");
-  const btnCerrarSesion = document.querySelector("#closeApp");
-
-  /* ----------------------- Variables de uso frecuente ----------------------- */
-  const apiUrlGetMe = "https://ctd-todo-api.herokuapp.com/v1/users/getMe";
-  const apiUrlTasks = "https://ctd-todo-api.herokuapp.com/v1/tasks";
-  const jwt = localStorage.getItem("token");
-
-  /* ---------------------------- Submit Formulario --------------------------- */
+  /* ---------------------------- Submit Formulario y login --------------------------- */
+  if (!jwt) {
+    location.href = "./index.html";
+  }
   formTasks.addEventListener("submit", (e) => e.preventDefault());
 
   /* --------------------------------- Resquests --------------------------------- */
 
-  // ▻▻▻▻▻ Obtener y mostrar nombre de usuario ---- ᓚᘏᗢ
-  obtenerNombreUsuario(apiUrlGetMe, jwt);
+  // ▻▻▻▻▻ Renderizar Nombre usuario ---- ᓚᘏᗢ
+  renderizarNombreUsuario(apiUrlGetMe, jwt);
 
   // ▻▻▻▻▻ Crear nueva tarea y respuesta al usuario en caso de error ---- ᓚᘏᗢ
-  btnNuevaTarea.addEventListener("click", function (e) {
+  btnNuevaTarea.addEventListener("click", (e) => {
     e.preventDefault();
-
-    const nuevaTareaObj = {
-      description: document.querySelector("#nuevaTarea").value,
-      completed: false,
-    };
-
-    if (nuevaTareaObj.description == "") {
-      errorMessage.innerText = "Ingrese tarea";
-      errorMessage.classList.remove("ocultar");
-      setTimeout(() => {
-        errorMessage.classList.add("ocultar");
-      }, 3000);
-    } else {
-      crearNuevaTarea(apiUrlTasks, jwt, nuevaTareaObj);
-    }
-
-    formTasks.reset();
+    nuevaTareaIngresada();
   });
 
   // ▻▻▻▻▻ Obtener y ordenar listas de tareas ---- ᓚᘏᗢ
   obtenerListaTareas(apiUrlTasks, jwt);
+
+  // ▻▻▻▻▻ Cerrar la aplicación ---- ᓚᘏᗢ
+  btnCerrarSesion.addEventListener("click", function () {
+    localStorage.removeItem("token");
+    localStorage.removeItem("listaTareas");
+    location.href = "./index.html";
+  });
+}); //cierre del window event
+
+/* -------------------------- Funcionalidades -------------------------- */
+
+function renderizarNombreUsuario(url, token) {
+  const settings = {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  };
+  fetch(url, settings)
+    .then((response) => response.json())
+    .then((datosUsuario) => {
+      localStorage.setItem("usuario", datosUsuario.firstName);
+      mostrarUsuario.innerText = datosUsuario.firstName;
+    });
+}
+
+function nuevaTareaIngresada() {
+  const nuevaTareaObj = {
+    description: document.querySelector("#nuevaTarea").value,
+    completed: false,
+  };
+
+  if (nuevaTareaObj.description == "") {
+    errorMessage.innerText = "Ingrese tarea";
+    errorMessage.classList.remove("ocultar");
+    setTimeout(() => {
+      errorMessage.classList.add("ocultar");
+    }, 3000);
+  } else {
+    crearNuevaTarea(apiUrlTasks, jwt, nuevaTareaObj);
+  }
+
+  formTasks.reset();
+}
+
+function crearNuevaTarea(url, token, objNorm) {
+  const settings = {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(objNorm),
+  };
+  fetch(url, settings)
+    .then((response) => response.json())
+    .then(() => {
+      obtenerListaTareas(apiUrlTasks, token);
+    });
+}
+
+function obtenerListaTareas(url, token) {
+  const settings = {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  };
+  fetch(url, settings)
+    .then((response) => response.json())
+    .then((lista) => {
+      renderizarPorTipoTarea(lista);
+    });
+}
+
+function renderizarPorTipoTarea(lista) {
   arrayTareasPendientes = [];
   arrayTareasTerminadas = [];
 
-  function ordenarListaTareas(lista) {
-    // Vaciamos los arrays para que los cambios no me afecten el renderizado de tareas
-    arrayTareasPendientes.splice(0, arrayTareasPendientes.length);
-    arrayTareasTerminadas.splice(0, arrayTareasTerminadas.length);
+  // Vaciamos los arrays para que los cambios no me afecten el renderizado de tareas
+  arrayTareasPendientes.splice(0, arrayTareasPendientes.length);
+  arrayTareasTerminadas.splice(0, arrayTareasTerminadas.length);
 
-    localStorage.setItem("listaTareas", lista);
-    lista.forEach((element) => {
-      if (element.completed == true) {
-        arrayTareasTerminadas.push(element);
-      } else arrayTareasPendientes.push(element);
-    });
-    arrayTareasPendientes.reverse();
-    arrayTareasTerminadas.reverse();
+  localStorage.setItem("listaTareas", lista);
+  lista.forEach((element) => {
+    if (element.completed == true) {
+      arrayTareasTerminadas.push(element);
+    } else arrayTareasPendientes.push(element);
+  });
+  arrayTareasPendientes.reverse();
+  arrayTareasTerminadas.reverse();
 
-    // ------------------  >>>  Tareas pendientes -------------------->>
-    tareasPendientes.innerHTML = "";
-    arrayTareasPendientes.forEach((tarea) => {
-      let date = new Date(tarea.createdAt);
-      tareasPendientes.innerHTML += `
+  // ------------------  >>>  Tareas pendientes -------------------->>
+  tareasPendientes.innerHTML = "";
+  arrayTareasPendientes.forEach((tarea) => {
+    let date = new Date(tarea.createdAt);
+    tareasPendientes.innerHTML += `
 <div>
 <li class="tarea">
 <div class="not-done" change id="${tarea.id}"></div>
@@ -84,101 +145,71 @@ fa-calendar-alt"></i> ${date.toLocaleDateString()}</p>
 <div>
 `;
 
-      // ------------------  >>>  Tareas Terminadas -------------------->>
-      tareasTerminadas.innerHTML = "";
-      arrayTareasTerminadas.forEach((tarea) => {
-        tareasTerminadas.innerHTML += `
-    <li class="tarea">
-  <div class="done"></div>
-  <div class="descripcion">
-    <p class="nombre">${tarea.description}</p>
-    <div>
-      <button>
-        <i id="${tarea.id}"
-          class="fas fa-undo-alt change"></i>
-      </button>
-      <button>
-        <i id="${tarea.id}" class="far fa-trash-alt"></i>
-      </button>
-    </div>
+    // ------------------  >>>  Tareas Terminadas -------------------->>
+    tareasTerminadas.innerHTML = "";
+    const newLocal = (tarea) => {
+      tareasTerminadas.innerHTML += `
+  <li class="tarea">
+<div class="done"></div>
+<div class="descripcion">
+  <p class="nombre">${tarea.description}</p>
+  <div>
+    <button>
+      <i id="${tarea.id}"
+        class="fas fa-undo-alt change"></i>
+    </button>
+    <button>
+      <i id="${tarea.id}" class="far fa-trash-alt"></i>
+    </button>
   </div>
+</div>
 </li>
-    `;
-      });
-    });
-  }
-
-    
-  // ▻▻▻▻▻ Eliminar tareas  ---- ᓚᘏᗢ
-
-  // ▻▻▻▻▻ Tildar tarea como realizada  ---- ᓚᘏᗢ
-
-  // ▻▻▻▻▻ Cerrar la aplicación ---- ᓚᘏᗢ
-  btnCerrarSesion.addEventListener("click", function () {
-    localStorage.removeItem("token");
-    localStorage.removeItem("listaTareas");
-    location.href = "./index.html";
+  `;
+    };
+    arrayTareasTerminadas.forEach(newLocal);
   });
-
-  /* -------------------------- Conexión al servidor -------------------------- */
-
-  function obtenerNombreUsuario(url, token) {
-    const settings = {
-      method: "GET",
-      headers: {
-        Authorization: token,
-      },
-    };
-    fetch(url, settings)
-      .then((response) => response.json())
-      .then((datosUsuario) => {
-        localStorage.setItem("usuario", datosUsuario.firstName);
-        mostrarUsuario.innerText = datosUsuario.firstName;
-      });
-  }
-
-  function crearNuevaTarea(url, token, objNorm) {
-    const settings = {
-      method: "POST",
-      headers: {
-        Authorization: token,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(objNorm),
-    };
-    fetch(url, settings)
-      .then((response) => response.json())
-      .then(() => {
-        obtenerListaTareas(url, token);
-      });
-  }
-
-  function obtenerListaTareas(url, token) {
-    const settings = {
-      method: "GET",
-      headers: {
-        Authorization: token,
-      },
-    };
-    fetch(url, settings)
-      .then((response) => response.json())
-      .then((lista) => {
-        ordenarListaTareas(lista);
-      });
-  }
+  obtenerIdTareaSeleccionada();
   if (arrayTareasPendientes.lentgh !== 0) {
     skeleton.remove();
   }
+}
 
-}); //cierre del window event
+function obtenerIdTareaSeleccionada() {
+  const escucharTareas = document.querySelectorAll("li.tarea");
+  escucharTareas.forEach((tarea) => {
+    tarea.addEventListener("click", function (e) {
+      let tareaSeleccionada = e.target;
+      if (tareaSeleccionada.id) {
+        localStorage.setItem("idTareaSeleccionada", tareaSeleccionada.id);
+        Swal.fire({
+          title: "¿Qué deseas hacer con esta tarea?",
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: "Modificar",
+          denyButtonText: `Eliminar`,
+          cancelButtonText: "Cancelar",
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            Swal.fire("Tarea modificada correctamente!");
+          } else if (result.isDenied) {
+            eliminarTarea(apiUrlDelete + `${tareaSeleccionada.id}`, jwt);
+            Swal.fire("Tarea eliminada correctamente!");
+          }
+        });
+      }
+    });
+  });
+}
 
-// ▻▻▻▻▻ Obtener una tarea en particular ---- ᓚᘏᗢ
-
-// tengo que capturar la tarea al hacer click sobre el circulo de la tarea...  ＼（〇_ｏ）／
-  const tareasNotDoneArray = document.querySelectorAll(".not-done");
-tareasNotDoneArray.forEach(tarea =>{
- tarea.addEventListener("click", function(e){
-   const tareaSeleccionada = e.target.id;
-   tareaSeleccionada.localStorage.setItem("tareaID")
- })
-});
+function eliminarTarea(url, token) {
+  const settings = {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  };
+  fetch(url, settings).then(() => {
+   obtenerListaTareas(apiUrlTasks, token);
+  });
+}
